d28989621d165b139043f9b661672480ebb57920
Ashok <vutlajava@gmail.com>
2025-08-26 20:14:00 -0500

    Added a Hello RestController

---
+++ src/main/java/com/example/springbootjenkins/HelloControll.java
@@ -0,0 +1,15 @@
+package com.example.springbootjenkins;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControll {
+
+    @GetMapping("hello")
+    public String hello() {
+
+        return "Hello";
+    }
+}
