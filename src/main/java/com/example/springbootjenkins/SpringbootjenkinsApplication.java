b7ea28bfea909086ce0690049e14d95d8208d442
Ashok <vutlajava@gmail.com>
2025-08-26 19:54:16 -0500

    first commit

---
+++ src/main/java/com/example/springbootjenkins/SpringbootjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootjenkins/SpringbootjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
